97deec8631c48a30989c4c1452e56cd647de909c
Adler Coelho Santos <adler.santos@ftd.com.br>
2019-08-08 21:29:22 -0300

    Creating Dev Schema.

---
+++ src/controllers/DevController.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const Dev = require('./../models/Dev');
+
+module.exports = {
+    async store(req, res) {
+        const { username: user } = req.body;
+        const url = `https://api.github.com/users/${user}`;
+        const usersExists = await Dev.findOne({ user });
+
+        if (usersExists) {
+            return res.json(usersExists);
+        }
+
+        const response = await axios.get(url);
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user,
+            bio,
+            avatar
+        });
+
+        return res.json(dev);
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
+
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.send('Hello Adler!')
 });
 
-routes.post('/dev', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/dev', DevController.store);
 
 module.exports = routes;
